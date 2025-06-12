81dcac9cfc752a4baca64fce1d9909c8d521f4a7
Omkar-somawanshi <omkarsomwanshi552003@gmail.com>
2025-06-12 17:36:28 +0530

    done

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/students", studentRoutes);
-
+PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("MongoDB connected");
-  app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+  app.listen(PORT, () => console.log("Server running on http://localhost:5000"));
 })
 .catch(err => console.error(err));
