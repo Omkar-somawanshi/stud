a4ed273658de55f37ed03816eb82007fd063917e
Omkar-somawanshi <omkarsomwanshi552003@gmail.com>
2025-06-13 10:30:31 +0530

    update

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/students", studentRoutes);
+app.use("/", (req, res) => {
+  res.send("Welcome to the Student Management API");
+});
 PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
